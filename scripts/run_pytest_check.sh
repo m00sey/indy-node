bc0ab0da0844dbfcfc0fae16221049964e7eb804
Kevin Griffin <griffin.kev@gmail.com>
2021-01-13 10:37:45 -0500

    new

--- scripts/run_pytest_check.sh
+++ scripts/run_pytest_check.sh
@@ -3,25 +3,26 @@
 modules=( "common" "node" )
 errs=0
 
-for m in "${modules[@]}"
-do
-    out=$(python3 scripts/pytest_mark_check.py indy_$m)
+# for m in "${modules[@]}"
+# do
+#     out=$(python3 scripts/pytest_mark_check.py indy_$m)
     
-    fin=$(echo $out | jq 'del(.status, .errors)')
+#     fin=$(echo $out | jq 'del(.status, .errors)')
     
-    echo $fin
+#     echo $fin
 
-    # result=$(echo $out | jq '.status')
+#     # result=$(echo $out | jq '.status')
 
-    # if [[ "$result" = "\"success\"" ]]; then
-    #     fin=$(echo $out | jq 'del(.status, .errors)')
-        echo "::set-output name=matrix-$m::$fin"
-    # else
-    #     ((errs=errs+1))
-    #     echo "$(echo $out | jq '.errors' | jq .[])"
-    # fi
-done
+#     # if [[ "$result" = "\"success\"" ]]; then
+#     #     fin=$(echo $out | jq 'del(.status, .errors)')
+#         # echo "::set-output name=matrix-$m::$fin"
+#     # else
+#     #     ((errs=errs+1))
+#     #     echo "$(echo $out | jq '.errors' | jq .[])"
+#     # fi
+# done
 
+echo "::set-output name=matrix-common::$(python scripts/pytest_mark_check.py indy_common)"
 
 if [[ errs -gt 0 ]]; then 
     exit 1
