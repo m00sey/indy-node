5c02512cc7ed97de4931fc99a814d36dc247cbcc
Kevin Griffin <griffin.kev@gmail.com>
2021-01-12 20:56:48 -0500

    new

---
+++ scripts/run_pytest_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+modules =( "common" "node" )
+
+for i in "${modules[@]}"
+do
+    echo "indy_$i"
+done
+
+echo $GITHUB_WORKSPACE
+
+# echo "::set-output name=matrix-common::$(python scripts/pytest_mark_check.py indy_common)"
+# echo "::set-output name=matrix-node::$(python scripts/pytest_mark_check.py indy_node)"
