6af94947350dc684cae152aafc7e3fd05c7fc7fe
Kevin Griffin <griffin.kev@gmail.com>
2021-01-13 11:30:03 -0500

    new

--- scripts/run_pytest_check.sh
+++ scripts/run_pytest_check.sh
@@ -3,26 +3,21 @@
 modules=( "common" "node" )
 errs=0
 
-# for m in "${modules[@]}"
-# do
-    out=$(python3 scripts/pytest_mark_check.py indy_common)
-    
-#     fin=$(echo $out | jq 'del(.status, .errors)')
-    
-#     echo $fin
+for m in "${modules[@]}"
+do
+    out=$(python3 scripts/pytest_mark_check.py indy_$m)
 
-#     # result=$(echo $out | jq '.status')
 
-#     # if [[ "$result" = "\"success\"" ]]; then
-#     #     fin=$(echo $out | jq 'del(.status, .errors)')
-#         # echo "::set-output name=matrix-$m::$fin"
-#     # else
-#     #     ((errs=errs+1))
-#     #     echo "$(echo $out | jq '.errors' | jq .[])"
-#     # fi
-# done
+    result=$(echo $out | jq '.status')
 
-echo "::set-output name=matrix-common::$out"
+    if [[ "$result" = "\"success\"" ]]; then
+        fin=$(echo $out | jq -c 'del(.status, .errors)')
+        echo "::set-output name=matrix-$m::$fin"
+    else
+        ((errs=errs+1))
+        echo "$(echo $out | jq '.errors' | jq .[])"
+    fi
+done
 
 if [[ errs -gt 0 ]]; then 
     exit 1
