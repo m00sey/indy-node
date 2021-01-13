4fb66cd9f62e4dc5ff320f07bd542659956169ab
Kevin Griffin <griffin.kev@gmail.com>
2021-01-12 23:36:15 -0500

    new

--- scripts/run_pytest_check.sh
+++ scripts/run_pytest_check.sh
@@ -7,12 +7,10 @@ for m in "${modules[@]}"
 do
     out=$(python3 scripts/pytest_mark_check.py indy_$m)
 
-    echo "$out"
-    
     result=$(echo $out | jq '.status')
 
     if [[ "$result" = "\"success\"" ]]; then
-        echo "::set-output name=matrix-$m::$(echo $out | jq 'del(.status)' | jq 'del(.errs)')"
+        echo "::set-output name=matrix-$m::$(echo $out)"
     else
         ((errs=errs+1))
         echo "$(echo $out | jq '.errors' | jq .[])"
